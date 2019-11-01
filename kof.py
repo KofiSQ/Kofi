67bdefcb990ff0b81bfb75f2462596ece4ce3864
KofiSQ <57196046+KofiSQ@users.noreply.github.com>
2019-11-01 10:29:23 -0400

    Add files via upload

---
+++ kof.py
@@ -0,0 +1,19 @@
+# Zigzag by Kofi
+# show a zig zig animation
+# Press ctrl-C to stop the program
+import time
+indentsize = 0 # how many spaces to indent
+DELAY = 0.08
+while True: # the main program loop
+    # Zig to the right 20 times:
+    B = 20
+
+    for i in range(B):
+        indentsize = indentsize + 1
+        print(' ' * indentsize + '########')
+        time.sleep(DELAY)# pause for milliseconds
+    # Zag to th left 20 times:
+    for i in range(B):
+        indentsize = indentsize - 1
+        print(' ' * indentsize + '########')
+        time.sleep(DELAY)  # pause for milliseconds
